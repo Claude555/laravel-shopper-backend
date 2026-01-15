46a763409c969708e2135012ec415db87bb37929
Claude555 <ibrahimclaude009@gmail.com>
2026-01-15 06:51:28 +0300

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['https://shopper-beta-vert.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
